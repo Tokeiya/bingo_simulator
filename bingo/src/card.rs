846a4b0e6cf04afe1ea49ac8988c59d64b034580
tokeiya3 <net_seed@hotmail.com>
2026-03-15 14:12:38 +0900

    before merge

--- bingo/src/card.rs
+++ bingo/src/card.rs
@@ -1,21 +1,26 @@
+use super::error::Result;
 use rand_core::Rng;
+use std::fmt::Display;
 
+#[derive(Debug)]
 pub struct Card([i8; 25]);
 
 impl Card {
 	pub fn new(rnd: &mut impl Rng) -> Self {
 		todo!()
 	}
+
+	pub fn set(&mut self, row: usize, col: usize) -> Result<()> {
+		todo!()
+	}
+
+	pub fn get(&self, row: usize, col: usize) -> Result<bool> {
+		todo!()
+	}
 }
 
-#[cfg(test)]
-mod test {
-	use super::*;
-	use colored::Colorize;
-	#[test]
-	fn hoge() {
-		let a = "hello";
-
-		println!("{}", a.blue());
+impl Display for Card {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		todo!()
 	}
 }
