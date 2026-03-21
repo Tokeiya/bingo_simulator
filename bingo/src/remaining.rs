adfed934814604736bd191c654aaa5f021d885fc
Tokeiya <net_seed@hotmail.com>
2026-03-21 17:58:03 +0900

    implement Remaining struct methods and add tests for row, column, and diagonal handling

--- bingo/src/remaining.rs
+++ bingo/src/remaining.rs
@@ -9,25 +9,57 @@ pub struct Remaining([u8; 12]);
 
 impl Remaining {
 	pub fn new() -> Self {
-		todo!()
+		Self([5; _])
 	}
 	pub fn row(&self, idx: usize) -> Result<usize> {
-		todo!()
+		if idx >= 5 {
+			Err(Error::RowOutOfRange(idx))
+		} else {
+			Ok(self.0[ROW_IDX[idx]] as usize)
+		}
 	}
 	pub fn column(&self, idx: usize) -> Result<usize> {
-		todo!()
+		if idx >= 5 {
+			Err(Error::ColumnOutOfRange(idx))
+		} else {
+			Ok(self.0[COL_IDX[idx]] as usize)
+		}
 	}
 	pub fn main_diagonal(&self) -> usize {
-		todo!()
+		self.0[MAIN_DIAGONAL] as usize
 	}
 	pub fn anti_diagonal(&self) -> usize {
-		todo!()
+		self.0[ANTI_DIAGONAL] as usize
 	}
 	fn decrement(&mut self, point: &Point) -> Result<()> {
-		todo!()
+		let row = ROW_IDX[point.row() as usize];
+		let col = COL_IDX[point.col() as usize];
+		let lies_main = point.lies_on_main_diagonal();
+		let lies_anti = point.lies_on_anti_diagonal();
+
+		if self.0[row] == 0 {
+			Err(Error::RemainingIsZero)
+		} else if self.0[col] == 0 {
+			Err(Error::RemainingIsZero)
+		} else if lies_main && self.0[MAIN_DIAGONAL] == 0 {
+			Err(Error::RemainingIsZero)
+		} else if lies_anti && self.0[ANTI_DIAGONAL] == 0 {
+			Err(Error::RemainingIsZero)
+		} else {
+			self.0[row] -= 1;
+			self.0[col] -= 1;
+			if lies_main {
+				self.0[MAIN_DIAGONAL] -= 1;
+			}
+			if lies_anti {
+				self.0[ANTI_DIAGONAL] -= 1;
+			}
+
+			Ok(())
+		}
 	}
 	fn view(&self) -> &[u8] {
-		todo!()
+		&self.0
 	}
 }
 
@@ -45,18 +77,34 @@ mod tests {
 	#[test]
 	fn row() {
 		let mut fixture = Remaining::new();
-		let mut cnt = 0;
+		let mut cnt = 5;
 
 		for i in ROW_IDX.iter() {
 			fixture.0[*i] = cnt;
 			cnt -= 1;
 		}
 
+		cnt = 5;
+
 		for i in 0..5 {
-			assert_eq!(fixture.row(i).unwrap(), i)
+			dbg!(i);
+			dbg!(fixture.row(i).unwrap());
+			assert_eq!(fixture.row(i).unwrap(), cnt.into());
+			cnt -= 1;
 		}
 
 		assert!(matches!(fixture.row(5),Err(Error::RowOutOfRange(x)) if x==5));
+	}
+
+	#[test]
+	fn col() {
+		let fixture = Remaining(std::array::from_fn(|x| x as u8));
+
+		for i in 0..5 {
+			assert_eq!(fixture.column(i).unwrap(), i + 5);
+		}
+
+		assert!(matches!(fixture.column(5), Err(Error::ColumnOutOfRange(x)) if x==5))
 	}
 
 	#[test]
@@ -75,7 +123,11 @@ mod tests {
 
 		for i in (0..5).rev() {
 			fixture.decrement(&p).unwrap();
-			assert_eq!(fixture.0[0], i);
+			assert_eq!(fixture.0[ROW_IDX[0]], i);
+		}
+
+		for (idx, cnt) in fixture.0.iter().enumerate() {
+			println!("[{idx}]={cnt}");
 		}
 
 		assert!(matches!(fixture.decrement(&p), Err(Error::RemainingIsZero)));
@@ -83,7 +135,7 @@ mod tests {
 
 	#[test]
 	fn view() {
-		let mut fixture = Remaining(std::array::from_fn(|x| x as u8));
+		let fixture = Remaining(std::array::from_fn(|x| x as u8));
 
 		assert_eq!(&fixture.0, fixture.view());
 	}
