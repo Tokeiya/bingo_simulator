a30adf3d9dd18f269b63562e16468a8014c9748c
tokeiya3@Home <net_seed@hotmai.com>
2026-03-22 22:12:07 +0900

    Refactor `main` logic: simplify accumulation, replace `roll` with `hall`, adjust bounds, and enhance output formatting.

--- simulator/src/bin/main.rs
+++ simulator/src/bin/main.rs
@@ -4,32 +4,18 @@ use rand_chacha::ChaCha20Rng;
 use rand_core::SeedableRng;
 
 fn main() {
-	let mut accum = [0usize; 75];
 	let mut rnd = ChaCha20Rng::from_seed([42; _]);
+	let mut accum = 0f64;
 
-	for i in 0..10000 {
-		accum[roll(&mut rnd)] += 1;
+	for i in 0..20 {
+		let (c, b) = hall(&mut rnd, 100);
+
+		accum += c as f64;
+
+		println!("round:{i} cnt:{c} tie:{b}");
 	}
 
-	for i in 0..accum.len() {
-		println!("{}:{}", i, accum[i]);
-	}
-}
-
-fn debug(rnd: &mut impl rand_core::Rng) {
-	let mut arr: [u8; 75] = std::array::from_fn(|i| (i + 1) as u8);
-	arr.shuffle(rnd);
-
-	let mut card = Card::new(rnd, true);
-	println!("{:?}", card.remaining().view());
-
-	card.set(11).unwrap();
-	card.set(16).unwrap();
-	card.set(49).unwrap();
-	card.set(70).unwrap();
-
-	println!("{}", card);
-	println!("{:?}", card.remaining().view());
+	println!("avg:{:.2}", accum / 100f64);
 }
 
 fn roll(rnd: &mut impl rand_core::Rng) -> usize {
@@ -49,10 +35,34 @@ fn roll(rnd: &mut impl rand_core::Rng) -> usize {
 		}
 	}
 
-	if cnt < 10 || cnt > 65 {
+	if cnt < 7 || cnt > 73 {
 		println!("CNT:{cnt}");
 		println!("{}\n\n", &card);
 	}
 
 	cnt
+}
+
+fn hall(rnd: &mut impl rand_core::Rng, num: usize) -> (usize, usize) {
+	let mut vec: Vec<Card> = (0..num).map(|_| Card::new(rnd, true)).collect();
+	let mut arr: [u8; 75] = std::array::from_fn(|i| (i + 1) as u8);
+	arr.shuffle(rnd);
+
+	let mut cnt = 0;
+
+	for ball in arr.into_iter() {
+		cnt += 1;
+
+		vec.iter_mut().for_each(|card| _ = card.set(ball).unwrap());
+		let bingo = vec
+			.iter()
+			.filter(|x| x.remaining().view().contains(&0))
+			.count();
+
+		if bingo != 0 {
+			return (cnt, bingo);
+		}
+	}
+
+	unreachable!()
 }
