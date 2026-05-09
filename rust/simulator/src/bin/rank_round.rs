5adc07cc124b72bc41c8504f6eba7bd14543318a
Tokeiya <net_seed@hotmail.com>
2026-05-09 18:23:58 +0900

    add `play` function skeleton and thread-local RNG

--- rust/simulator/src/bin/rank_round.rs
+++ rust/simulator/src/bin/rank_round.rs
@@ -1,1 +1,16 @@
+use bingo::Card;
+use rand::prelude::SliceRandom;
+use rand_chacha::ChaCha20Rng;
+use rayon::prelude::*;
+use simulator::aggregation::*;
+use simulator::rng_helper::generate;
+use std::cell;
+use std::io::Write;
+
+thread_local! {
+	static RNG:cell::RefCell<ChaCha20Rng> = cell::RefCell::new(generate());
+}
+
 fn main() {}
+
+fn play(n: usize) {}
