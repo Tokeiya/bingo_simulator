be91c5d506a8368907cbc965bec4c29f9f9a14ad
tokeiya3@Home <net_seed@hotmai.com>
2026-05-14 21:52:49 +0900

    Refactor `Data` structure: remove `SimulationIterator`, simplify iteration logic, and update unit tests accordingly.

--- rust/simulator/src/rank_round/iter.rs
+++ rust/simulator/src/rank_round/iter.rs
@@ -6,57 +6,16 @@ pub struct Data {
 	pub current_iteration: usize,
 }
 
-pub struct SimulationIterator {
-	play_id: usize,
-	iteration: usize,
-	current_iteration: usize,
-	player_step: usize,
-	player_limit: usize,
-	player: usize,
-}
-
-impl SimulationIterator {
-	pub fn new(
-		initial_player: usize,
-		player_count: usize,
-		player_step: usize,
-		iteration: usize,
-	) -> Self {
+impl Data {
+	pub fn new(play_id: usize, initial_player: usize, step: usize, total_iteration: usize) -> Self {
+		let iteration = play_id % total_iteration;
+		let player = initial_player + (play_id / total_iteration) * step;
 		Self {
-			play_id: 0,
-			iteration,
-			player_limit: initial_player + player_count * player_step,
-			player_step,
-			player: initial_player,
-			current_iteration: 0,
+			play_id,
+			player,
+			total_iteration,
+			current_iteration: iteration,
 		}
-	}
-}
-
-impl Iterator for SimulationIterator {
-	type Item = Data;
-
-	fn next(&mut self) -> Option<Self::Item> {
-		let dta = Data {
-			play_id: self.play_id,
-			player: self.player,
-			total_iteration: self.iteration,
-			current_iteration: self.current_iteration,
-		};
-
-		if self.player >= self.player_limit {
-			return None;
-		}
-
-		self.play_id += 1;
-		self.current_iteration += 1;
-
-		if self.current_iteration >= self.iteration {
-			self.player += self.player_step;
-			self.current_iteration = 0;
-		}
-
-		Some(dta)
 	}
 }
 
@@ -65,18 +24,18 @@ mod test {
 	use super::*;
 
 	#[test]
-	fn next() {
-		let mut fixture = SimulationIterator::new(10, 10, 10, 10);
-		let mut count = 0usize;
+	fn data() {
+		let mut cnt = 0;
 
-		for player in (1..=10).map(|x| x * 10) {
+		for p in (1..=10).map(|x| x * 10) {
 			for i in 0..10 {
-				let dta = fixture.next().unwrap();
-				assert_eq!(dta.player, player);
-				assert_eq!(dta.current_iteration, i);
-				assert_eq!(dta.total_iteration, 10);
-				assert_eq!(dta.play_id, count);
-				count += 1;
+				let fixture = Data::new(cnt, 10, 10, 10);
+				dbg!(&fixture);
+				assert_eq!(fixture.player, p);
+				assert_eq!(fixture.current_iteration, i);
+				assert_eq!(fixture.total_iteration, 10);
+				assert_eq!(fixture.play_id, cnt);
+				cnt += 1;
 			}
 		}
 	}
